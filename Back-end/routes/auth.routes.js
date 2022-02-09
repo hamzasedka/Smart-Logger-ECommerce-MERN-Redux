f7c264cf4415372dfc70316e2cb30b0b832bb7da
aymen2709 <aymen.abid4070@gmail.com>
2022-02-09 10:12:38 +0100

    Fixed cors error

--- Back-end/routes/auth.routes.js
+++ Back-end/routes/auth.routes.js
@@ -2,11 +2,13 @@ const { verifySignUp } = require("../middlewares");
 const controller = require("../controllers/auth.controller");
 module.exports = function(app) {
   app.use(function(req, res, next) {
-    res.header(
-      "Access-Control-Allow-Headers",
-      "x-access-token, Origin, Content-Type, Accept",
-      "Access-Control-Allow-Origin: http://localhost:3000"
-    );
+    res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    // res.header(
+    //   "Access-Control-Allow-Headers",
+    //   "x-access-token, Origin, Content-Type, Accept",
+    //   "Access-Control-Allow-Origin: http://localhost:3000"
+    // );
     next();
   });
   app.post(
